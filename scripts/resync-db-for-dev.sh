5ba0b65d0f1ae4750b1b507716e36ef2d146ad4d
Colan Schwartz <colan@58704.no-reply.drupal.org>
2014-10-21 17:39:53 -0400

    Remove the user-creation bits as devs can simply use "drush uli".

--- scripts/resync-db-for-dev.sh
+++ scripts/resync-db-for-dev.sh
@@ -133,9 +133,5 @@ $SUDO $CHOWN -R $USER_WEB $($DRUSH dd $DESTINATION:%files)
 $ECHO "Run any periodic tasks that should be run..."
 $DRUSH $DESTINATION cron
 
-$ECHO "Creating an administrator user with your username..."
-$DRUSH $DESTINATION user-create $USER --mail="$USER@example.com" --password="letmein"
-$DRUSH $DESTINATION user-add-role administrator --name=$USER
-
 $ECHO "All done!"
 $ECHO "End time: $($DATE +%T)"
